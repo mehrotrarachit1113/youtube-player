dbb3dd3945fcacb678497f281e20959c81563975
mehrotrarachit1113 <rachit.mehrotra1113@gmail.com>
2018-11-12 21:04:38 +0530

    React Youtube App.......................................

---
+++ src/Components/video_list_item.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const VideoListItem = (props)=>{
+  const video = props.video;
+  const imageURL = video.snippet.thumbnails.default.url;
+  const onVideoSelect = props.onVideoSelect;
+
+  return(
+    <li onClick = {()=>onVideoSelect(video)} className = "list-group-item">
+      <div className = "video-list media">
+        <div className = "media-left">
+          <img className = "media-object" src = {imageURL} alt = "" />
+        </div>
+        <div className = "media-body">
+          <div className = "media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default VideoListItem;
